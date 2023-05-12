fbf2d85cd4f5f825725d0a02bf8aa8c1e035e05e
Shivkanta Nagargoje <125877363+shivkantaN@users.noreply.github.com>
2023-05-12 08:17:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function threeSum(arr, target) {
 // write your code here
+	
+   let binary = "";
+  while (num > 0) {
+    binary = (num % 2) + binary;
+    num = Math.floor(num / 2);
+  }
+  
+  return binary;
+}
+ 
+module.exports = decimalToBinary;
+
   
 }
 
